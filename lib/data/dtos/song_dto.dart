29f89fbb9e66bfef935a2e3c420a8b0ef03732a2
Hak_Kimly <kimly.hak@student.cadt.edu.kh>
2026-03-26 15:32:52 +0700

    W9-01 – Fetch and display songs in the library screen

--- lib/data/dtos/song_dto.dart
+++ lib/data/dtos/song_dto.dart
@@ -1,21 +1,23 @@
-    import '../../model/songs/song.dart';
+import '../../model/songs/song.dart';
 
 class SongDto {
   static const String idKey = 'id';
-  static const String titleKey = 'name';
-  static const String artistKey = 'artists';
-  static const String durationKey = 'durationMs';   // in ms
+  static const String titleKey = 'title';
+  static const String artistIdKey = 'artistId';
+  static const String durationKey = 'duration'; // in ms
+  static const String imageUriKey = 'imageUrl';
 
-  static Song fromJson(Map<String, dynamic> json) {
-    assert(json[idKey] is String);
+  static Song fromJson(Map<String, dynamic> json, {required String id}) {
     assert(json[titleKey] is String);
-    assert(json[artistKey] is String);
+    assert(json[artistIdKey] is String);
     assert(json[durationKey] is int);
+    assert(json[imageUriKey] is String);
 
     return Song(
-      id: json[idKey],
+      id: id,
       title: json[titleKey],
-      artist: json[artistKey],
+      artistId: json[artistIdKey],
+      imageUrl: json[imageUriKey],
       duration: Duration(milliseconds: json[durationKey]),
     );
   }
@@ -25,8 +27,9 @@ class SongDto {
     return {
       idKey: song.id,
       titleKey: song.title,
-      artistKey: song.artist,
+      artistIdKey: song.artistId,
       durationKey: song.duration.inMilliseconds,
+      imageUriKey: song.imageUrl.toString(),
     };
   }
 }
--- lib/data/repositories/songs/song_repository_mock.dart
+++ lib/data/repositories/songs/song_repository_mock.dart
@@ -8,45 +8,49 @@ class SongRepositoryMock implements SongRepository {
     Song(
       id: 's1',
       title: 'Mock Song 1',
-      artist: 'Mock Artist',
+      artistId: 'Mock Artist',
+      imageUrl: 'https://images.unsplash.com/photo-1511379938547-c1f69419868d',
       duration: const Duration(minutes: 2, seconds: 50),
+      
     ),
     Song(
       id: 's2',
       title: 'Mock Song 2',
-      artist: 'Mock Artist',
+      artistId: 'Mock Artist',
+      imageUrl: 'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91',
       duration: const Duration(minutes: 3, seconds: 20),
     ),
     Song(
       id: 's3',
       title: 'Mock Song 3',
-      artist: 'Mock Artist',
+      artistId: 'Mock Artist',
+      imageUrl: 'https://images.unsplash.com/photo-1497032205916-ac775f0649ae',
       duration: const Duration(minutes: 3, seconds: 20),
     ),
     Song(
       id: 's4',
       title: 'Mock Song 4',
-      artist: 'Mock Artist',
+      artistId: 'Mock Artist',
+      imageUrl: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330',
       duration: const Duration(minutes: 3, seconds: 20),
     ),
     Song(
       id: 's5',
       title: 'Mock Song 5',
-      artist: 'Mock Artist',
+      artistId: 'Mock Artist',
+      imageUrl: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e',
       duration: const Duration(minutes: 3, seconds: 20),
     ),
   ];
 
   @override
   Future<List<Song>> fetchSongs() async {
-    return Future.delayed(Duration(seconds: 4), () {
-      throw Exception("G3 and G4 the class is finished");
-    });
+    return Future.delayed(const Duration(seconds: 1), () => _songs);
   }
 
   @override
   Future<Song?> fetchSongById(String id) async {
-    return Future.delayed(Duration(seconds: 4), () {
+    return Future.delayed(Duration(seconds: 1), () {
       return _songs.firstWhere(
         (song) => song.id == id,
         orElse: () => throw Exception("No song with id $id in the database"),
